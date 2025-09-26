4b77ff82996bd0f280793f6e4ef69ca22a1d9ccc
Chems <labadi.chemsou@gmail.com>
2025-09-26 10:33:52 +0200

    tp2_exo7

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from test import Chien
+import numpy as np
+import matplotlib.pyplot as plt 
 
-c = Chien()
-c.afficher()
+from utils import display_graph_from_dict  
+
+def main():
+    # Définition du graphe sous forme de dictionnaire
+    graph = {
+        "A": ["B"],     # A pointe vers B
+        "B": [],        # B n'a pas de successeurs
+        "C": ["A", "B"],# C pointe vers A et B
+        "D": ["C"],     # D pointe vers C
+        "E": ["D", "C"] # E pointe vers D et C
+    }
+
+    # Afficher le graphe
+    display_graph_from_dict(graph)
+    
+main()
